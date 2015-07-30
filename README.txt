fc4a48131b847a435681b99b97f10a1c90e6a3b6
Simo <simoessahli@gmail.com>
2015-07-30 17:24:41 +0100

    Zray Extension for CodeIgniter
    CodeIgniter plugin provides valuable information on codeIgniter
    applications, making debugging easy and simple. Z-Ray gives a complete
    picture of your CodeIgniter environment - including all loaded views,
    models, helpers, configuration files and stored sessions.

---
+++ README.txt
@@ -0,0 +1,13 @@
+Zend Server Zray Plugin for CodeIgniter
+=======================================
+
+CodeIgniter plugin provides valuable information on codeIgniter applications, making debugging easy and simple. Z-Ray gives a complete picture of your CodeIgniter environment - including all loaded views, models, helpers, configuration files and stored sessions.
+
+- **Installation**: shows the steps to complete the installation,
+- **Configuration**: shows the configuration files variables,
+- **Views**: shows information on currently loaded CodeIgniter views, including their application file path,
+- **Hooks**: displays the active Hooks,
+- **Hooks**: shows the loaded libraries,
+- **Models**: shows the loaded models,
+- **Helpers**: display the loaded helpers,
+- **Request**: provides information on the current route of the CodeIgniter application, such as the controller name, the action name and the parameters.
---
+++ zray/zray.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace ZRayExtension;
+
+class CodeIgniter
+{
+
+    private $ci = null;
+
+    const SUPPORTED_CI_VERSION = '2.2';
+
+    public function init($context, &$storage)
+    {
+    
+    	if(null !== $this->ci)
+	        return;
+	        
+        $storage['installation']['current_version'] = CI_VERSION;
+        $storage['installation']['supported_version'] = static::SUPPORTED_CI_VERSION;
+        $storage['installation']['is_supported_version'] = true;
+        
+        if (version_compare(CI_VERSION, static::SUPPORTED_CI_VERSION, '<')) {
+	        $storage['installation']['is_supported_version'] = false;
+            return; //this extension support only C.I version >= 2.2.0
+        }
+        
+        $this->ci =& get_instance();
+            
+        $storage['configuration'] = $this->loadConfiguration();
+        $storage['models'] = $this->loadModels();
+        $storage['libraries'] = $this->loadLibraries();
+        $storage['helpers'] = $this->loadHelpers();
+        $storage['hooks'] = $this->loadHooks();
+        $storage['views'] = $this->loadViews();
+        $storage['request'] = $this->getControllerInfo();
+    }
+
+    private function loadConfiguration()
+    {
+        $config = array();
+
+        foreach ($this->ci->config->config as $name => $value) {
+            $value = is_array($value) ? '<pre>' . print_r($value, true) . '</pre>' : $value;
+            $config[$name] = array('name' => $name, 'value' => $value);
+        }
+
+        return $config;
+    }
+
+    private function loadViews()
+    {
+        $views = array();
+        if (method_exists($this->ci->load, 'get_views')) {
+            $views = $this->ci->load->get_views();
+        }
+
+        $base_path = substr(str_replace(SYSDIR, '', BASEPATH), 0, -1);
+
+        $_views = array();
+
+        foreach ($views as $path => $view) {
+            $path = str_replace($base_path, '', $path);
+            $_views[$path] = array('name' => $view, 'path' => $path);
+        }
+
+        return $_views;
+    }
+
+    private function loadModels()
+    {
+        $models = array();
+        if (method_exists($this->ci->load, 'get_models')) {
+            $models = $this->ci->load->get_models();
+        }
+
+        return array_map(function ($item) {
+            return array('name' => $item);
+        }, $models);
+    }
+
+    private function loadLibraries()
+    {
+        $loaded_libraries =& is_loaded();
+
+        return array_map(function ($item) {
+            return array('name' => $item);
+        }, $loaded_libraries);
+    }
+
+    private function loadHelpers()
+    {
+        $helpers = array();
+
+        if (method_exists($this->ci->load, 'get_helpers')) {
+            $helpers = $this->ci->load->get_helpers();
+        }
+
+        return array_map(function ($item) {
+            return array('name' => $item);
+        }, array_keys($helpers));
+    }
+
+    private function loadHooks()
+    {
+        $hooks = array();
+        if (!$this->ci->hooks->enabled) {
+            return array();
+        }
+
+        foreach ($this->ci->hooks->hooks as $hook_point => $_hooks) {
+            if (!isset($_hooks[0]))
+                $_hooks = array($_hooks);
+
+            foreach ($_hooks as $hook) {
+                $hooks[$hook_point] = array_merge(array('event' => $hook_point), $hook);
+            }
+        }
+
+        return $hooks;
+    }
+
+    private function getControllerInfo()
+    {
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $controller = $this->ci->router->class;
+        $action = $this->ci->router->method;
+        $parameters = $this->ci->input->{$method}();
+        $key = $controller . '::' . $action;
+
+        $info = array();
+        $info[$key] = array(
+            'controller' => ucfirst($controller),
+            'action' => $action,
+            'method' => strtoupper($method),
+            'parameters' => $parameters
+        );
+        
+        return $info;
+    }
+}
+
+$zre = new \ZRayExtension('CodeIgniter');
+$zre->setEnabledAfter('MY_Loader::_ci_load');
+
+$codeIgniter = new CodeIgniter();
+
+$zre->traceFunction('CI_Output::_display', function () {}, array($codeIgniter, 'init'));
+
+$zre->setMetadata(array(
+    'logo' => __DIR__ . DIRECTORY_SEPARATOR . 'logo.png',
+));
